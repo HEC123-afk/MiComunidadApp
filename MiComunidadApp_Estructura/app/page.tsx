68243a1f0880d9817acac016ec923577e581980e
HECTOR PINTO <marcelopampino@gmail.com>
2025-05-02 20:21:20 -0400

    next.config.js
    Subida inicial de Mi Comunidad App
    
    
    Signed-off-by: HECTOR PINTO <marcelopampino@gmail.com>

---
+++ MiComunidadApp_Estructura/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Bienvenido a Mi Comunidad App</h1>
+      <p>Tu plataforma de administración vecinal.</p>
+    </main>
+  );
+}
